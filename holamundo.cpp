2fd8d10c948a080a35ea2c528b14a4dd3aedf7be
victor <victoresp.alumn@cescristorey.com>
2020-11-10 21:48:51 +0100

    Version 1.0

--- holamundo.cpp
+++ holamundo.cpp
@@ -3,18 +3,14 @@
 
 using namespace std;
 
-int hola(int hola) {
+void hola() {
 
 	cout << “Hola mundo” << endl;		
-	return hola;
 }
 
 int main() {
 
-    string hola = NULL;
-    int holaa = 9;
-	
-    holaa= hola(holaa);
+    hola();
 
     return 0;
 
